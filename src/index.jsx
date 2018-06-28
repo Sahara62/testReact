74e85e2e08c97c67a69c561410214e5d689986ea
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-06-28 16:48:57 +0900

    tugi

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
 class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  constructor(props) {
+    super(props)
+    this.state = { message: 'something' }
+  }
+
+  onChange(e) {
+     this.setState( {message: e.target.value} )
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange = { this.onChange.bind(this) } />
+        <p>{ this.state.message }</p>
+      </div>
+    )
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('app'))
